fb3e4bb75406aca352524e8ba5e1261eb86d2992
Manish Giri <manish9dec@gmail.com>
2018-12-04 17:29:42 -0500

    New Commit

--- MapkitApp/GameScene.swift
+++ MapkitApp/GameScene.swift
@@ -8,42 +8,23 @@
 
 import SpriteKit
 import GameplayKit
+import MapKit
+import CoreLocation
 
 class GameScene: SKScene {
+   
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+   
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
+   
     override func sceneDidLoad() {
 
-        self.lastUpdateTime = 0
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
+   // }
     
-    
+ /*
     func touchDown(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -88,12 +69,12 @@ class GameScene: SKScene {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
-    
+    */
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
         // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
+       /* if (self.lastUpdateTime == 0) {
             self.lastUpdateTime = currentTime
         }
         
@@ -105,6 +86,12 @@ class GameScene: SKScene {
             entity.update(deltaTime: dt)
         }
         
-        self.lastUpdateTime = currentTime
+        self.lastUpdateTime = currentTime*/
     }
+    
+    
+    
 }
+
+
+
--- MapkitApp/GameViewController.swift
+++ MapkitApp/GameViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import MapKit
+import CoreLocation
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var mapKitViews: MKMapView!
+    let locationManager = CLLocationManager()
+    let regionMeters :Double = 10000
+    var previousLocation: CLLocation?
+    
+    @IBOutlet weak var addressLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,8 +31,8 @@ class GameViewController: UIViewController {
             if let sceneNode = scene.rootNode as! GameScene? {
                 
                 // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
+               // sceneNode.entities = scene.entities
+                //sceneNode.graphs = scene.graphs
                 
                 // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
@@ -41,7 +49,64 @@ class GameViewController: UIViewController {
             }
         }
     }
-
+    
+    
+    func centerViewOnLocation(){
+        
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 10000, longitudinalMeters: 10000)
+            
+            mapKitViews.setRegion(region, animated: true)
+        }
+    }
+    func checkLocationAuthorization(){
+        
+        switch CLLocationManager.authorizationStatus() {
+            
+        case .authorizedAlways: break
+        case .authorizedWhenInUse:
+            startTrackingUserLocation()
+        case .denied: break
+        case .notDetermined: locationManager.requestWhenInUseAuthorization()
+        case .restricted: break
+            
+            
+        }
+        
+    }
+    
+    func startTrackingUserLocation(){
+        
+        mapKitViews.showsUserLocation = true
+        centerViewOnLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapKitViews)
+        
+    }
+    func setupLocationManager(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+    }
+    func checkLocationServices(){
+        
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        }else {
+            
+        }
+    }
+    
+    
+    func getCenterLocation(for mkmapView: MKMapView )-> CLLocation {
+      let lat = mkmapView.centerCoordinate.latitude
+    let lon = mkmapView.centerCoordinate.longitude
+    
+    return CLLocation(latitude: lat, longitude: lon)
+        
+    }
+    
     override var shouldAutorotate: Bool {
         return true
     }
@@ -58,3 +123,55 @@ class GameViewController: UIViewController {
         return true
     }
 }
+extension GameViewController: CLLocationManagerDelegate {
+    
+   /* func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionMeters, longitudinalMeters: regionMeters)
+        mapKitViews.setRegion(region, animated: true)
+        }*/
+        
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+     checkLocationAuthorization()
+}
+    
+}
+    extension GameViewController: MKMapViewDelegate {
+    
+        func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+            let center = getCenterLocation(for: mapView)
+            
+            let geoCoder = CLGeocoder()
+            
+            geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+                guard let previousLocation = self?.previousLocation else { return }
+                
+                guard center.distance(from: previousLocation) > 50 else {return }
+                self?.previousLocation = center
+                guard self != nil else {return}
+                
+                if let _ = error {
+                    return
+                }
+                
+                guard let placemark = placemarks?.first else {
+                    return
+                }
+                
+                let streetNumber = placemark.subThoroughfare
+                let streetName = placemark.thoroughfare
+                
+                DispatchQueue.main.async {
+               
+                    self?.addressLabel.text = "\(streetNumber ?? "0")\(streetName ?? "0")"
+                }
+                
+        }
+            
+            
+        }
+        
+    }
+
+
